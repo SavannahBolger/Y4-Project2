dbca28fe850856c0f39aec245925f85fe0069e99
SavannahBolger <C00215747@itcarlow.ie>
2020-02-14 12:43:43 +0000

    functional unity platform

--- Final Project/Assets/Scripts/rotate.cs
+++ Final Project/Assets/Scripts/rotate.cs
@@ -4,52 +4,50 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour {
 
-    public float newYAngle;
-    public float oldYAngle;
-    public float newXAngle;
-    public float oldXAngle;
+    public float x = 0.0f;
+    public float z = 0.0f;
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        x = transform.rotation.x * 100;
+        z = transform.rotation.z * 100;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            StopAllCoroutines();
-            newYAngle = oldYAngle - 90f;
-            StartCoroutine(Rotate(newXAngle,newYAngle));
-            oldYAngle = newYAngle;
+            if (z > -34.0f)
+            {
+                transform.Rotate(0, 0, -1);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            StopAllCoroutines();
-            newYAngle = oldYAngle + 90f;
-            StartCoroutine(Rotate(newXAngle,newYAngle));
-            oldYAngle = newYAngle;
+            if (z < 34.0f)
+            {
+                transform.Rotate(0, 0, 1);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            StopAllCoroutines();
-            newXAngle = oldYAngle + 90f;
-            StartCoroutine(Rotate(newXAngle, newYAngle));
-            oldYAngle = newXAngle;
+            if (x < 34.0f)
+            {
+                transform.Rotate(1, 0, 0);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            StopAllCoroutines();
-            newXAngle = oldXAngle - 90f;
-            StartCoroutine(Rotate(newXAngle, newYAngle));
-            oldXAngle = newXAngle;
+            if (x > -34.0f)
+            {
+                transform.Rotate(-1, 0, 0);
+            }
         }
     }
 
-    IEnumerator Rotate(float targetAngleX, float targetAngleY)
-    {
-        while (transform.rotation.y != targetAngleY || transform.rotation.x != targetAngleX)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(targetAngleX, targetAngleY, 0f), 3f * Time.deltaTime);
-            yield return null;
-        }
-        transform.rotation = Quaternion.Euler(targetAngleX, targetAngleY, 0f);
-        yield return null;
-    }
 }
