5b8d447476ff00732d88fe7148c562a1f0852d3f
SavannahBolger <C00215747@itcarlow.ie>
2020-02-21 08:03:48 +0000

    leap controller added to project

---
+++ Final Project/Assets/Scripts/InteractionHand.cs
@@ -0,0 +1,49 @@
+﻿using Leap;
+using Leap.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHand : MonoBehaviour {
+
+	Controller controller = new Controller();
+	Frame frame;
+	Frame previous;
+	Hand hand;
+	// Use this for initialization
+	void Start () {
+		if(controller.isConnected())
+        {
+			frame = controller.Frame();
+			previous = controller.Frame(1);
+			if (frame.Hands.Count > 0)
+			{
+				List<Hand> hands = frame.Hands;
+				hand = frame.Hands[0];
+			}
+		}
+	}
+
+	public Hand SourceHand
+	{
+		set
+		{
+			hand = value;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (hand != null)
+		{
+			
+			// Use the rotation from the live data
+			var handRotation = hand.Rotation.ToQuaternion();
+
+			// Transform the copied hand so that it's centered on the current hands position and matches it's rotation.
+			transform(hand.PalmPosition.ToVector3(), handRotation);
+		}
+
+	}
+}
