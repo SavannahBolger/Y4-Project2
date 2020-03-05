c5afeec9351d3453cbeda2baf8b4da00075936c4
SavannahBolger <C00215747@itcarlow.ie>
2020-03-05 21:42:36 +0000

    working hand control

--- Final Project/Assets/Scripts/InteractionHand.cs
+++ Final Project/Assets/Scripts/InteractionHand.cs
@@ -7,50 +7,54 @@ using UnityEngine;
 
 public class InteractionHand : MonoBehaviour {
 
-    //   Controller controller;
-    //Frame frame;
-    //Frame previous;
-    //Hand hand;
-    //   // Use this for initialization
-    //   void Start()
-    //   {
-    //       controller = new Controller();
-    //       frame = controller.Frame();
-    //       previous = controller.Frame(1);
-    //       if (frame.Hands.Count > 0)
-    //       {
-    //           List<Hand> hands = frame.Hands;
-    //           hand = frame.Hands[0];
-    //       }
+    Controller controller;
+    Frame frame;
+    Frame previous;
+    Hand hand;
+   // Use this for initialization
 
-    //   }
-    /*
-         //public Hand SourceHand
-         //{
-         //	set
-         //	{
-         //		hand = value;
-         //	}
-         //}
+   void Start()
+    {
 
-         //// Update is called once per frame
-         //void Update () {
-         //	if (hand != null)
-         //	{
+       controller = new Controller();
 
-         //		// Use the rotation from the live data
-         //		var handRotationX = hand.Rotation.x;
-         //		var handRotationZ = hand.Rotation.z;
-         //           Debug.Log("hand detect");
-         //		// Transform the copied hand so that it's centered on the current hands position and matches it's rotation.
-         //		transform.Rotate(handRotationX, 0, handRotationZ);
-         //	}
+       frame = controller.Frame();
 
-         //}
+       previous = controller.Frame(1);
+        if (frame.Hands.Count > 0)
+        {
+        List<Hand> hands = frame.Hands;
+        hand = frame.Hands[0];
+    }
 
-         /// <summary>
-         /// The Leap controller.
-         /// </summary>
+    }
+    
+         public Hand SourceHand
+         {
+         	set
+         	{
+         		hand = value;
+         	}
+         }
+
+         // Update is called once per frame
+         void Update () {
+         	if (hand != null)
+         	{
+
+         		// Use the rotation from the live data
+         		var handRotationX = hand.Rotation.x;
+         		var handRotationZ = hand.Rotation.z;
+                    Debug.Log("hand detect");
+         		// Transform the copied hand so that it's centered on the current hands position and matches it's rotation.
+         		transform.Rotate(handRotationX, 0, handRotationZ);
+         	}
+
+         }
+
+         // <summary>
+         // The Leap controller.
+         // </summary>
          Controller controller;
 
          /// <summary>
@@ -83,9 +87,9 @@ public class InteractionHand : MonoBehaviour {
              get { return (controller != null && controller.Devices.Count > 0 && controller.IsConnected); }
          }
 
-         /// <summary>
-         /// Awake is called when the script instance is being loaded.
-         /// </summary>
+         // <summary>
+         // Awake is called when the script instance is being loaded.
+         // </summary>
          void Awake()
          {
              controller = new Controller();
@@ -108,44 +112,44 @@ public class InteractionHand : MonoBehaviour {
 
              transform.rotation = Quaternion.Euler(mainHand.Direction.Pitch, mainHand.Direction.Yaw, mainHand.PalmNormal.Roll);
 
-             // For relative orientation
-             // transform.rotation *= Quaternion.Euler( mainHand.Direction.Pitch, mainHand.Direction.Yaw, mainHand.PalmNormal.Roll );
+              //For relative orientation
+              transform.rotation *= Quaternion.Euler( mainHand.Direction.Pitch, mainHand.Direction.Yaw, mainHand.PalmNormal.Roll );
 
-         }*/
-    Controller controller;
-    float HandPalmPitch;
-    float HandPalmYaw;
-    float HandPalmRoll;
-    float HandWristRot;
+         }
+    //Controller controller;
+    //float HandPalmPitch;
+    //float HandPalmYaw;
+    //float HandPalmRoll;
+    //float HandWristRot;
 
-    void Start()
-    {
+    //void Start()
+    //{
 
-    }
+    //}
 
-    void Update()
-    {
-        controller = new Controller();
-        Frame frame = controller.Frame();
-        List<Hand> hands = frame.Hands;
-        if (frame.Hands.Count > 0)
-        {
-            Hand fristHand = hands[0];
-        }
+    //void Update()
+    //{
+    //    controller = new Controller();
+    //    Frame frame = controller.Frame();
+    //    List<Hand> hands = frame.Hands;
+    //    if (frame.Hands.Count > 0)
+    //    {
+    //        Hand fristHand = hands[0];
+    //    }
 
-        HandPalmPitch = hands[0].PalmNormal.Pitch;
-        HandPalmRoll = hands[0].PalmNormal.Roll;
-        HandPalmYaw = hands[0].PalmNormal.Yaw;
+    //    HandPalmPitch = hands[0].PalmNormal.Pitch;
+    //    HandPalmRoll = hands[0].PalmNormal.Roll;
+    //    HandPalmYaw = hands[0].PalmNormal.Yaw;
 
-        HandWristRot = hands[0].WristPosition.Pitch;
+    //    HandWristRot = hands[0].WristPosition.Pitch;
 
-        Debug.Log("Pitch :" + HandPalmPitch);
-        Debug.Log("Roll :" + HandPalmRoll);
-        Debug.Log("Yaw :" + HandPalmYaw);
+    //    Debug.Log("Pitch :" + HandPalmPitch);
+    //    Debug.Log("Roll :" + HandPalmRoll);
+    //    Debug.Log("Yaw :" + HandPalmYaw);
 
-        if(HandPalmYaw > -2f && HandPalmYaw < 3.5f)
-        {
-            transform.rotation *= Quaternion.Euler( HandPalmPitch, HandPalmYaw, HandPalmRoll);
-        }
-    }
+    //    if(HandPalmYaw > -2f && HandPalmYaw < 3.5f)
+    //    {
+    //        transform.rotation *= Quaternion.Euler( HandPalmPitch, 0, HandPalmRoll);
+    //    }
+    //}
 }
