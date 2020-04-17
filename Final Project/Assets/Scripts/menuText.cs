c255137937ac66a4bd6afb87e7630a80a3109657
SavannahBolger <C00215747@itcarlow.ie>
2020-04-17 11:43:09 +0100

    game screens added and textures

---
+++ Final Project/Assets/Scripts/menuText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI
+
+public class menuText : MonoBehaviour {
+
+	//public Text menuText;
+	//public Text playText;
+	//public Text optionsText;
+	//public Text quitText;
+
+	public bool isGame;
+	public bool isOptions;
+	public bool isQuit;
+
+	// Use this for initialization
+	void Start () {
+		menuText.text = "Main Menu";
+		playText.text = "Play Game";
+		optionsText.text = "Settings";
+		quitText.text = "Quit Game";
+	}
+
+	// Update is called once per frame
+	void OnMouseUp()
+	{
+
+		if (isGame)
+		{
+			Application.LoadLevel("Level 1");
+		}
+
+		if (isOptions)
+		{
+			Application.LoadLevel("Options");
+		}
+
+		if (isQuit)
+		{
+			Application.Quit();
+		}
+	}
+}
