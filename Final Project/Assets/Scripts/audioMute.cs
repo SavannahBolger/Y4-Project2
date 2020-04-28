c09657cdb0f37b5513c4f5c0061d6dc3b36127bd
SavannahBolger <C00215747@itcarlow.ie>
2020-04-28 23:55:02 +0100

    audio and mute option

---
+++ Final Project/Assets/Scripts/audioMute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioMute : MonoBehaviour {
+
+	AudioSource audioSource;
+	// Use this for initialization
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void Mute()
+	{
+		audioSource.mute = !audioSource.mute;
+	}
+}
